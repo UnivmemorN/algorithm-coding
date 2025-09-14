687683206a874929e452b02316b6b7a3706ce684
unicornshjl <3159669749@qq.com>
2025-09-14 23:21:00 +0800

    Create P3128.cpp

---
+++ 树上差分/P3128.cpp
@@ -0,0 +1,110 @@
+点差分统计数量的模板题
+同时本题的代码使用了 Tarjan 算法来求解 lca 时间复杂度是线性的 但是要求询问 必须可以离线!
+
+
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<random>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include<set>
+#include<cstdlib>
+#include<vector>
+#include<iomanip>
+#include<utility>
+#include<unordered_map>
+#include<unordered_set>
+#include<bitset> 
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+constexpr int maxn = 6e5 + 10;
+constexpr int maxm = 6e6 + 10;
+constexpr int intinf = 1e9 + 10;
+constexpr int mod = 998244353;
+constexpr ll mods[] = { 998244353,1000000007,1000000009 };
+constexpr ll inf = 1e15 + 10;
+constexpr int dx[] = { 1,-1,0,0 };
+constexpr int dy[] = { 0,0,1,-1 };
+constexpr ld eps = 1e-12;
+using namespace std;
+int n, q, sum[maxn], dep[maxn], f[maxn], father[maxn], ans[maxn];
+bool vis[maxn];
+vector<int> edge[maxn];
+vector<pair<int,int>> ask[maxn];
+
+int find(int x) {
+	return(x == f[x] ? x : f[x] = find(f[x]));
+}
+
+void add(int u, int v) {
+	edge[u].push_back(v);
+}
+
+void init() {
+	cin >> n >> q;
+	for (int i = 1; i <= n; i++)
+		f[i] = i;
+	for (int i = 1; i < n; i++) {
+		int u, v;
+		cin >> u >> v;
+		add(u, v); add(v, u);
+	}
+	for (int i = 1; i <= q; i++) {
+		int u, v;
+		cin >> u >> v;
+		sum[u]++; sum[v]++;
+		ask[u].push_back({ v,i });
+		ask[v].push_back({ u,i });
+	}
+}
+
+void dfs(int u, int fa) {
+	vis[u] = 1;
+	father[u] = fa;
+	for (auto v : edge[u]) {
+		if (v == fa) continue;
+		dfs(v, u);
+		f[v] = u;
+	}
+	for (auto [v, num] : ask[u])
+		if (vis[v])
+			ans[num] = find(v);
+}
+
+void doit(int u, int fa) {
+	for (auto v : edge[u]) {
+		if (v == fa) continue;
+		doit(v, u);
+		sum[u] += sum[v];
+	}
+}
+
+void solve() {
+	for (int i = 1; i <= q; i++) {
+		sum[ans[i]]--;
+		sum[father[ans[i]]]--;
+	}
+	doit(1, 0);
+}
+
+void query() {
+	int res = 0;
+ 	for (int i = 1; i <= n; i++)
+		res = max(res, sum[i]);
+	cout << res << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	init();
+	dfs(1, 0);
+	solve();
+	query();
+	return 0;
+}
